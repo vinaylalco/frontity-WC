8427bf591372ca4708809015fe9edb83a0e9cb3c
Vinay Lal <onethruten@gmail.com>
2022-07-07 22:03:43 +0530

    tutorial_part2.3

---
+++ packages/my-first-theme/src/components/index.js
@@ -0,0 +1,13 @@
+// File: /packages/my-first-theme/src/components/index.js
+
+import React from "react"
+
+const Root = () => {
+  return (
+    <>
+      <h1>Hello Frontity</h1>
+    </>
+  )
+}
+
+export default Root
